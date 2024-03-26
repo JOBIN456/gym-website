11b3090dd904bfd1674a2f8825525943e97cc390
Jobin Jose <109533232+JOBIN456@users.noreply.github.com>
2024-03-26 13:50:35 +0530

    pages with buttons

--- gym_website/gym_app/urls.py
+++ gym_website/gym_app/urls.py
@@ -4,4 +4,9 @@ from .import views
 
 urlpatterns = [
     path("",views.home,name="home"),
+    path("about",views.about,name="about"),
+    path("facilities",views.facilities,name="facilities"),
+    path("contact",views.contact,name="contact"),
+    path("trainers",views.trainers,name="trainers"),
+
 ]
--- gym_website/gym_app/views.py
+++ gym_website/gym_app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"home.html")
+
+def about(request):
+    return render(request,"about.html")
+def facilities(request):
+    return render(request,"facilities.html")
+def contact(request):
+    return render(request,"contact.html")
+def trainers(request):
+    return render(request,"trainers.html")
