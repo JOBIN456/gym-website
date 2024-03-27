dfe0331eed464000f71b70fccfccd7725137d1e1
Jobin Jose <109533232+JOBIN456@users.noreply.github.com>
2024-03-27 13:40:44 +0530

    contact-page

--- gym_website/gym_app/views.py
+++ gym_website/gym_app/views.py
@@ -9,6 +9,19 @@ def about(request):
 def facilities(request):
     return render(request,"facilities.html")
 def contact(request):
+    if request.method == 'POST':
+        name=request.POST.get('full-name')
+        email=request.POST.get('email')
+        message=request.POST.get('message')
+
+        email_info={
+            'name':name,
+            'email':email,
+            'message':message
+
+        }
+
+        print(email_info)
     return render(request,"contact.html")
 def trainers(request):
     return render(request,"trainers.html")
