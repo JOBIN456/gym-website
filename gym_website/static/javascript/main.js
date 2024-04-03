2351f7e6f4255adb4edc4f2b506c5b7cf06adf50
Jobin Jose <109533232+JOBIN456@users.noreply.github.com>
2024-04-03 11:17:24 +0530

    scroll animation using intersection observer api

--- gym_website/static/javascript/main.js
+++ gym_website/static/javascript/main.js
@@ -7,4 +7,76 @@
 //     });
 
 
+const the_animation = document.querySelectorAll('.trainer-card')
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('train-card-animation')
+        }
+            else {
+                entry.target.classList.remove('train-card-animation')
+            }
+        
+    })
+},
+   { threshold: 0.5
+   }
+   
+   );
+
+
+  for (let i = 0; i < the_animation.length; i++) {
+   const elements = the_animation[i];
+
+    observer.observe(elements);
+  } 
+
+
+
+// const the_animation_1= document.querySelectorAll('.our-trainers')
+// const the_observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting)
+//         {
+//             entry.target.classList.add('our-trainers-animation')
+
+//         }
+//         else{
+//             entry.target.classList.remove('our-trainers-animation')
+//         }
+//     })
+// },
+// {
+//     threshold:0.5
+
+// }
+//   );
+//   for (let i = 0; i < the_animation_1.length; i++) {
+//     const elements = the_animation_1[i];
+ 
+//     the_observer .observe(elements);
+//    } 
+ 
+
+
+
+
+
+
+
+const the_animation_1 = document.querySelector('.our-trainers');
+
+const the_observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('our-trainers-animation');
+        } else {
+            entry.target.classList.remove('our-trainers-animation');
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+the_observer.observe(the_animation_1);
