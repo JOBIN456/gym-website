e4e8f0efc411809bb08d31245f7dd6d3d3da707c
Jobin Jose <109533232+JOBIN456@users.noreply.github.com>
2024-04-04 12:09:07 +0530

    AOS-animations

--- gym_website/static/javascript/main.js
+++ gym_website/static/javascript/main.js
@@ -7,32 +7,32 @@
 //     });
 
 
-const the_animation = document.querySelectorAll('.trainer-card')
+// const the_animation = document.querySelectorAll('.trainer-card')
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('train-card-animation')
-        }
-            else {
-                entry.target.classList.remove('train-card-animation')
-            }
-        if(entry.isIntersecting){
-            observer.unobserve(entry.target)
-        }
-    })
-},
-   { threshold: 0.5
-   }
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('train-card-animation')
+//         }
+//             else {
+//                 entry.target.classList.remove('train-card-animation')
+//             }
+//         if(entry.isIntersecting){
+//             observer.unobserve(entry.target)
+//         }
+//     })
+// },
+//    { threshold: 0.5
+//    }
    
-   );
+//    );
 
 
-  for (let i = 0; i < the_animation.length; i++) {
-   const elements = the_animation[i];
+//   for (let i = 0; i < the_animation.length; i++) {
+//    const elements = the_animation[i];
 
-    observer.observe(elements);
-  } 
+//     observer.observe(elements);
+//   } 
 
 
 
@@ -44,25 +44,25 @@ const observer = new IntersectionObserver((entries) => {
 
 
 
-const the_animation_1 = document.querySelector('.our-trainers');
+// const the_animation_1 = document.querySelector('.our-trainers');
 
-const the_observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('our-trainers-animation');
+// const the_observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('our-trainers-animation');
 
-        } 
-        else {
+//         } 
+//         else {
 
-            entry.target.classList.remove('our-trainers-animation');
-        }
-    //    if (entry.isIntersecting)
-    //    {
-    //     the_observer.unobserve(entry.target)
-    //    }
-    });
-}, {
-    threshold: 0.5
-});
+//             entry.target.classList.remove('our-trainers-animation');
+//         }
+//     //    if (entry.isIntersecting)
+//     //    {
+//     //     the_observer.unobserve(entry.target)
+//     //    }
+//     });
+// }, {
+//     threshold: 0.5
+// });
 
-the_observer.observe(the_animation_1);
+// the_observer.observe(the_animation_1);
